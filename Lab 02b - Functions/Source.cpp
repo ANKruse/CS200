361f5c552af09f9d232c17059e18fdc368a37be5
ANKruse <ANKruse@users.noreply.github.com>
2017-07-10 18:49:53 -0500

    Add files via upload

---
+++ Lab 02b - Functions/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cmath> // needed for square root
+using namespace std;
+
+float GetDistance(float x1, float y1, float x2, float y2)
+{
+	float a = (x2 - x1);
+	float b = (y2 - y1);
+	return sqrt(pow(a, 2)+pow(b, 2));
+}
+int main()
+{
+	while (true)
+	{
+		float x1, y1, x2, y2;
+
+		cout << "First coordinate pair, enter x and y: ";
+		cin >> x1 >> y1;
+
+		cout << "Second coordinate pair, enter x and y: ";
+		cin >> x2 >> y2;
+
+		float distance = GetDistance(x1, y1, x2, y2);
+		cout << "Distance: " << distance << endl;
+
+		cout << endl << endl;
+	}
+	while (true);
+	return 0;
+}
